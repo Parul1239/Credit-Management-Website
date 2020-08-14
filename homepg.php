9c26c86e16b47fad6a24cd13cd8f32bb20183314
Parul Keshri <43788007+Parul1239@users.noreply.github.com>
2020-08-14 17:55:44 +0530

    Home Page

---
+++ homepg.php
@@ -0,0 +1,42 @@
+ <?php
+		  
+$con=mysqli_connect("localhost","root","","test");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link type="text/css" rel="stylesheet" href="css/home.css" >
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class="logo">
+	 <a href="homepg.php"><img src="images/img4.jpg" alt="logo"></a>
+</div>
+<p><b>Easy Transfer<b></p>
+<div class="header">
+<br>
+<h2>Welcome To</h2>
+<h1><strong>Credit Management</strong></h1>
+<form action="users.php">
+<button class='button1' >Get Started</button>
+</form>
+</div>
+</body>
+<div class="fixed-footer">
+        <div class="container">Copyright &copy; Credit Management Website by Parul Keshri   
+		<a href="#" class="fa fa-facebook"></a>
+		<a href="#" class="fa fa-twitter"></a>
+		<a href="#" class="fa fa-google"></a>
+		<a href="#" class="fa fa-linkedin"></a>
+		</div>
+    </div>
+
+</html>
